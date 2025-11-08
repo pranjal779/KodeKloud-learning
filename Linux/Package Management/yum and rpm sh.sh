226d3fd8661f319f1a4fe7d4ae42c56094e1639f
Pranjal Shukla <pranjal779@gmail.com>
2025-11-08 23:55:56 +0530

    Create yum and rpm sh.sh

---
+++ Linux/Package Management/yum and rpm sh.sh
@@ -0,0 +1,14 @@
+yum install httpd
+
+# or
+yum install httpd -y
+
+yum repolist
+
+yum provides scp # command name as argument ? scp is the argument
+
+yum remove httpd
+
+yum update telnet
+
+yum update # to update all the package in the system and check for new ones
