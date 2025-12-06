bdb5a71557b16c2309b10ad7fc113ddb6ffb008a
Pranjal Shukla <pranjal779@gmail.com>
2025-12-06 07:55:06 +0530

    Create DOCKER.sh

---
+++ DOCKER/DOCKER.sh
@@ -0,0 +1,14 @@
+# Run - Start a container
+docker run nginx
+
+# ps - List containers
+docker ps
+
+docker ps -a
+
+docker stop <imageName>
+# or
+docker stop <ContainerID>
+
+docker ps -a # to check if the container is stopped
+
